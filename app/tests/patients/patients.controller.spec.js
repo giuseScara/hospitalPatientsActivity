b11f904f3a360aa1dd6817f868a6bd532282d1d5
Giuseppe <giuse.scara@gmail.com>
2016-12-11 22:07:06 +0100

    Add karma conf

---
+++ app/tests/patients/patients.controller.spec.js
@@ -0,0 +1,17 @@
+describe("Controller Test", function () {
+  var mockScope = {};
+  var controller;
+
+  beforeEach(module("AppModule"));
+
+  /*beforeEach(angular.mock.module.inject(function ($controller, $rootScope) {
+    mockScope = $rootScope.$new();
+
+    controller = $controller("PatientsController", {
+      //$scope = mockScope;
+    });
+  }));*/
+
+
+
+});
